8fd7c71b610c07b3d1f0965f9a5251aa3a8b25c7
konika-singh <konikasingh1996@gmail.com>
2025-05-24 22:46:16 +0530

    nginx server install

--- Infra/terraform/main.tf
+++ Infra/terraform/main.tf
@@ -6,6 +6,14 @@ resource "aws_instance" "devops_ec2" {
   ami           = "ami-084568db4383264d4" # Amazon Linux 2 (example)
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install nginx1 -y
+              sudo systemctl start nginx
+              sudo systemctl enable nginx
+            EOF
+
   tags = {
     Name = "DevOps-Practice-Instance"
   }
